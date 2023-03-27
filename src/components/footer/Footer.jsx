5aa22a8a603a9070c755af53f2c29108605c8507
Gonzalo Airasca <gonzaloairasca@gmail.com>
2023-03-27 18:39:43 -0300

    commit con footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import logo from "../../assets/recursos/logo.png";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <img src={logo} alt="logo la hilda embotelladora de agua" />
-        <h2>EMBOTELLADORA DE AGUA LA HILDA S.R.L</h2>
-        <h3>Fransisco Villa 3105 - B° Los Plátanosz</h3>
+    <footer className="bg-[#0080C6] ">
+      <div className="flex justify-center flex-col items-center py-8">
+        <h1 className="f-Alex-Brush text-5xl text-[#F7100C] font-semibold text-shadow textstroke">
+          La Hilda
+        </h1>
+
+        <h3 className="text-white font-semibold text-base f-mplus text-center">
+          EMBOTELLADORA DE AGUA LA HILDA S.R.L
+        </h3>
+        <hr className="w-10/12 my-6" />
+        <h5 className="text-blue-200 font-light text-sm f-mplus text-center px-4">
+          Laprida 2882 - B° Alto Alberdi, Córdoba capital. | Tel:(+54) 9 351
+          5937318
+        </h5>
+        <h6 className="text-sm text-center font-light text-blue-200 px-4 mt-4">
+          AGUAS LA HILDA © 2023 | DISEÑO Y DESARROLLO WEB{" "}
+          <a className="underline" href="https://gonzaloairascaweb.com.ar">
+            {">>"}GONZALO AIRASCA WEB
+          </a>
+        </h6>
       </div>
     </footer>
   );
