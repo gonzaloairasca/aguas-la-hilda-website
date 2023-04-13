2ca65807536b8062549cbdcd935fe07a217cb9c8
Gonzalo Airasca <gonzaloairasca@gmail.com>
2023-04-13 11:20:23 -0300

    commit con imagenes sin fondo y bg ligthblue

--- src/components/products/Productos.jsx
+++ src/components/products/Productos.jsx
@@ -5,11 +5,11 @@ import ProductosRetornables from "./ProductosRetornables";
 
 const Productos = () => {
   return (
-    <section className=" pt-10">
+    <section className="bg-gradient-to-b from-[#52c1fd54] to-[#52c1fd2a] pt-10">
       <h2 className="bg-[#39B5FF] text-shadow-separador w-max ml-4  px-2 py-1 f-mplus font-bold text-2xl text-white rounded-[3px] text-center mt-12 shadow-portada lg:text-3xl lg:ml-12 lg:py-2 lg:px-3">
         NUESTROS PRODUCTOS
       </h2>
-      <div>
+      <div className="">
         <h4 className="bg-[#39B5FF] text-shadow-separador w-max f-mplus font-bold text-xs text-white rounded-[3px] p-1 shadow-portada mx-4 mt-12 mb-4 lg:ml-12 lg:text-base">
           LINEA DE MESA
         </h4>
@@ -17,7 +17,7 @@ const Productos = () => {
           <ProductosDeMesa data={data} />
         </div>
       </div>
-      <div className="bg-productosRetornables p-px pb-10">
+      <div className=" p-px pb-10">
         <h3 className="bg-[#39B5FF] text-shadow-separador w-max f-mplus font-bold text-xs text-white rounded-[3px] p-1 shadow-portada  mx-4 mt-12 mb-4 lg:ml-12 lg:text-base">
           LINEA RETORNABLE
         </h3>
